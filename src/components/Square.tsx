19cb9368467b82fbd24f6e8bb025526e1776f3bd
Shinkrbs <kerbydolamoscs@gmail.com>
2025-09-15 14:02:06 +0800

    Setup square component

---
+++ src/components/Square.tsx
@@ -0,0 +1,15 @@
+interface SquareProps {
+  value: string | null;
+  onSquareClick: () => void;
+}
+
+export const Square = ({ value, onSquareClick }: SquareProps) => {
+
+    return (
+    <div>
+       <button className="square" onClick={onSquareClick}>
+      {value}
+    </button>
+    </div>
+  )
+}
